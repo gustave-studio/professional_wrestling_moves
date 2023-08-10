54435f920bf8e814e84f3e7af5095894fae7f3bf
gustave-studio <gustave0222@gmail.com>
2023-08-11 05:26:39 +0900

    デプロイ用の設定

--- diagnose_project/settings.py
+++ diagnose_project/settings.py
@@ -113,3 +113,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / "images"
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
